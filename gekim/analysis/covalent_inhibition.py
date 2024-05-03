1f894bc8d71c9398ff2d7b4926dd258f546fd32a
kghaby <61133492+kghaby@users.noreply.github.com>
2024-05-02 19:57:07 -0500

    fitting.x and fit xlim

--- gekim/analysis/covalent_inhibition.py
+++ gekim/analysis/covalent_inhibition.py
@@ -25,19 +25,20 @@ def occ_final_wrt_t(t,kobs,Etot,uplim=1):
     '''
     return uplim*Etot*(1-np.e**(-kobs*t))
 
-def kobs_uplim_fit_to_occ_final_wrt_t(t: np.array, occ_final: np.array, nondefault_params: dict = None): 
+def kobs_uplim_fit_to_occ_final_wrt_t(t: np.array, occ_final: np.array, nondefault_params: dict = None, xlim=None): 
     '''
     Fit kobs to the first order occupancy over time.
 
     Args:
         t: Array of timepoints.
         occ_final: Array of observed occupancy, i.e. concentration.
-        params (dict, optional): A structured dictionary of parameters with 'fixed','guess', and 'bound' keys. Include any params to update the default.
+        nondefault_params (dict): A structured dictionary of parameters with 'fixed','guess', and 'bound' keys. Include any params to update the default.
             default_params = {
                 "kobs": {"fix": None, "guess": 0.01, "bounds": (0,np.inf)}, # Observed rate constant
                 "Etot": {"fix": None, "guess": 1, "bounds": (0,np.inf)},    # Total concentration of E over all species
                 "uplim": {"fix": None, "guess": 1, "bounds": (0,np.inf)},   # Scales the upper limit of the curve
             }
+        xlim (tuple): Limits for the time points considered in the fit (min_t, max_t).
 
     Returns:
         an instance of the FitOutput class
@@ -60,13 +61,18 @@ def kobs_uplim_fit_to_occ_final_wrt_t(t: np.array, occ_final: np.array, nondefau
 
     p0, bounds, param_order, fixed_params = fitting._extract_fit_info(params)
 
+    if xlim:
+        indices = (t >= xlim[0]) & (t <= xlim[1])
+        t = t[indices]
+        occ_final = occ_final[indices]
+
     def fitting_adapter(t, *fitting_params):
         all_params = {**fixed_params, **dict(zip(param_order, fitting_params))}
         return occ_final_wrt_t(t,all_params["kobs"],all_params["Etot"],uplim=all_params["uplim"])
 
     popt, pcov = curve_fit(fitting_adapter, t, occ_final, p0=p0, bounds=tuple(bounds))
     fitted_data = fitting_adapter(t, *popt)
-    fit_output = fitting._prepare_output(popt, pcov, param_order, fitted_data, occ_final)
+    fit_output = fitting._prepare_output(popt, pcov, param_order, t, fitted_data, occ_final)
 
     return fit_output
 
@@ -90,20 +96,21 @@ def occ_total_wrt_t(t,kobs,concI0,KI,Etot,uplim=1):
     FO = 1/(1+(KI/concI0)) # Equilibrium occupancy of reversible portion
     return uplim*Etot*(1-(1-FO)*(np.e**(-kobs*t)))
 
-def kobs_KI_uplim_fit_to_occ_total_wrt_t(t: np.array, occ_tot: np.array, nondefault_params: dict = None): 
+def kobs_KI_uplim_fit_to_occ_total_wrt_t(t: np.array, occ_tot: np.array, nondefault_params: dict = None, xlim=None): 
     '''
     Fit kobs and KI to the total occupancy of all bound states over time, assuming fast reversible binding equilibrated at t=0.
 
     Args:
         t: Array of timepoints.
         occ_tot: Array of total occupancy.
-        params = {
+        nondefault_params = {
             "kobs": {"fix": None, "guess": 0.01, "bounds": (0,np.inf)},
             "concI0": {"fix": None, "guess": 100, "bounds": (0,np.inf)},
             "KI": {"fix": None, "guess": 10, "bounds": (0,np.inf)},
             "Etot": {"fix": None, "guess": 1, "bounds": (0,np.inf)},
             "uplim": {"fix": None, "guess": 1, "bounds": (0,1)},        # Scales the upper limit of the curve
         }
+        xlim (tuple): Limits for the time points considered in the fit (min_t, max_t).
 
     Returns:
         an instance of the FitOutput class
@@ -122,13 +129,18 @@ def kobs_KI_uplim_fit_to_occ_total_wrt_t(t: np.array, occ_tot: np.array, nondefa
 
     p0, bounds, param_order, fixed_params = fitting._extract_fit_info(params)
 
+    if xlim:
+        indices = (t >= xlim[0]) & (t <= xlim[1])
+        t = t[indices]
+        occ_tot = occ_tot[indices]
+
     def fitting_adapter(t, *fitting_params):
         all_params = {**fixed_params, **dict(zip(param_order, fitting_params))}
         return occ_total_wrt_t(t,all_params["kobs"],all_params["concI0"],all_params["KI"],all_params["Etot"],uplim=all_params["uplim"])
 
     popt, pcov = curve_fit(fitting_adapter, t, occ_tot, p0=p0, bounds=tuple(bounds))
     fitted_data = fitting_adapter(t, *popt)
-    fit_output = fitting._prepare_output(popt, pcov, param_order, fitted_data, occ_tot)
+    fit_output = fitting._prepare_output(popt, pcov, param_order, t, fitted_data, occ_tot)
 
     return fit_output   
 
@@ -147,20 +159,21 @@ def kobs_wrt_concI0(concI0,KI,kinact,n=1):
     '''
     return kinact/(1+(KI/concI0)**n)
 
-def KI_kinact_n_fit_to_kobs_wrt_concI0(concI0: np.array, kobs: np.array, nondefault_params: dict = None):
+def KI_kinact_n_fit_to_kobs_wrt_concI0(concI0: np.array, kobs: np.array, nondefault_params: dict = None, xlim=None):
     """
     Fit parameters (KI, kinact, n) to kobs with respect to concI0 using a structured dictionary for parameters.
 
     Args:
         concI0: Array of initial concentrations of the inhibitor.
         kobs: Array of observed rate constants.
-        params (dict, optional): A structured dictionary of parameters with 'fixed','guess', and 'bound' keys. Include any params to update the default.
+        nondefault_params (dict, optional): A structured dictionary of parameters with 'fixed','guess', and 'bound' keys. Include any params to update the default.
             default_params = {
                 "KI": {"fix": None, "guess": 100, "bounds": (0,np.inf)},
                 "kinact": {"fix": None, "guess": 0.01, "bounds": (0,np.inf)},
                 "n": {"fix": 1, "guess": 1, "bounds": (-np.inf,np.inf)}, # fix overrides guess, so set fix to None if you wish to include this 
             }
-
+        xlim (tuple): Limits for the concI0 points considered in the fit (min_concI0, max_concI0).
+        
     Returns:
         an instance of the FitOutput class
     """
@@ -176,13 +189,18 @@ def KI_kinact_n_fit_to_kobs_wrt_concI0(concI0: np.array, kobs: np.array, nondefa
 
     p0, bounds, param_order, fixed_params = fitting._extract_fit_info(params)
 
+    if xlim:
+        indices = (concI0 >= xlim[0]) & (concI0 <= xlim[1])
+        concI0 = concI0[indices]
+        kobs = kobs[indices]
+
     def fitting_adapter(concI0, *fitting_params):
         all_params = {**fixed_params, **dict(zip(param_order, fitting_params))}
         return kobs_wrt_concI0(concI0, all_params["KI"], all_params["kinact"], all_params["n"])
 
     popt, pcov = curve_fit(fitting_adapter, concI0, kobs, p0=p0, bounds=tuple(bounds))
     fitted_data = fitting_adapter(concI0, *popt)
-    fit_output = fitting._prepare_output(popt, pcov, param_order, fitted_data, kobs)
+    fit_output = fitting._prepare_output(popt, pcov, param_order, concI0, fitted_data, kobs)
 
     return fit_output
 

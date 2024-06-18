0f0db3792444dbb8af433a8dc1b415d4463c8c51
kghaby <61133492+kghaby@users.noreply.github.com>
2024-06-18 16:16:58 -0500

    0.5.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='GeKiM', 
-    version='0.5.2', 
+    version='0.5.3', 
     author='Kyle Ghaby', 
     author_email='kyleghaby@gmail.com',  
     description='Generalized Kinetic Modeler: A Python package for modeling arbitrary kinetic schemes.',  
